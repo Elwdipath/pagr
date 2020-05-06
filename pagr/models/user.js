1d66cc9b9989acc19d3c3351f5e3b386f5180b22
BRANDEN PATTEN <patten.branden@gmail.com>
2020-05-06 01:12:47 -0400

    Added Express and set up routes to manage Users in the database, along with a model for a user

---
+++ pagr/models/user.js
@@ -0,0 +1,14 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+  username: { type: String, required: true },
+  password: { type: String, required: true },
+  firstName: { type: String, required: true },
+  lastName: { type: String, required: true },
+  isAdmin: { type: Boolean }
+});
+
+const User = mongoose.model("User", userSchema);
+
+module.exports = User;
