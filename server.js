90f1a0c16b31e5f7a47e53e288fd293ef3bdc40e
BRANDEN PATTEN <patten.branden@gmail.com>
2020-05-06 06:24:09 -0400

    added some passport files, added requirements and middleware to server.js || Needs additional configuring.

--- server.js
+++ server.js
@@ -1,32 +1,38 @@
 const express = require("express");
-const routes = require("./routes");
 const PORT = process.env.PORT || 3001;
 const mongoose = require("mongoose");
 const app = express();
-const morgan = require("morgan")
+const morgan = require("morgan");
+const passport = require("./passport/passport")
+const session = require("express-session");
 
 // Define middleware here
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+//morgan for route tracing && debugging
+app.use(morgan("combined"));
+morgan("combined");
 
-app.use(morgan('combined'))
-morgan('combined')
-
-morgan(':remote-addr :method :url')
+morgan(":remote-addr :method :url");
 
 morgan(function (tokens, req, res) {
-  return req.method + ' ' + req.url
-})
+  return req.method + " " + req.url;
+});
+// User authentication using passport
+app.use(
+  session({ secret: "some secrets", resave: false, saveUninitialized: true })
+);
+app.use(passport.initialize());
+app.use(passport.session());
+
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
 
-
-
 // Define API routes here
-app.use(routes);
+require("./routes")(app, passport);
 
 // Connect to the Mongo DB
 mongoose
