2503c9596a525845a1dc4d6237615187f3f507d0
Robert Finkley <rfinkley82@gmail.com>
2020-05-08 18:51:24 -0400

    Updated home page

--- client/src/pages/home/index.js
+++ client/src/pages/home/index.js
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 import { Col, Row, Container } from "../../components/Grid";
 import Nav from "../../components/Nav";
 import Jumbotron from "../../components/Jumbotron";
-import FormBtn from "../../components/Form";
+import { FormBtn } from "../../components/Form";
 
 
 
@@ -13,9 +13,20 @@ class Home extends Component {
     return (
       <div className="wrapper">
         <Nav />
+        <Container />
         <Jumbotron>
-          <p className="text-center">Secure reliable communication when you need it. Learn how your organization can imporve on-call workflows with Pagr.</p>
+          <Row>
+            <Col size="sm-12">
+              <p className="text-center">Secure reliable communication when you need it. Learn how your organization can imporve on-call workflows with Pagr.</p>
+            </Col>
+          </Row>
+          <Row center="true">
+            <Col size="sm=12">
+              <a href="/sign-up" className="btn btn-success">Get Started</a>
+            </Col>
+          </Row>
         </Jumbotron>
+        <Container />
       </div>
     );
   }
