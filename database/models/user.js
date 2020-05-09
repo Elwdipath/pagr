6a44ddb1cc35ebd176d32ada0b24a0ea8ecefeac
Branden Patten <57732191+pattenbranden@users.noreply.github.com>
2020-05-09 13:34:04 -0400

    Update user.js

--- database/models/user.js
+++ database/models/user.js
@@ -5,8 +5,9 @@ const bcrypt = require("bcryptjs")
 const userSchema = new Schema({
   username: { type: String, required: true },
   password: { type: String, required: true },
-  // firstName: { type: String, required: true },
-  // lastName: { type: String, required: true },
+  email: { type: String, required: true },
+  firstName: { type: String, required: true },
+  lastName: { type: String, required: true },
   isAdmin: { type: Boolean }
 });
 
