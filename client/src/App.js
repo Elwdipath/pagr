ebe3c9e225055cef39b5c5a8e58a27eded93e568
Robert Finkley <rfinkley82@gmail.com>
2020-05-09 13:21:52 -0400

    Removed create-react-app css file on App.js file

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import './App.css';
 import Home from "./pages/home/";
+import SignUp from "./pages/signup";
 import Admin from "./pages/admin";
 import User from "./pages/user";
 import NoMatch from "./pages/noMatch";
@@ -17,6 +17,7 @@ function App() {
       <div>
         <Switch>
           <Route exact path="/" component={Home} />
+          <Route exact path="/signup" component={SignUp} />
           <Route exact path="/admin" component={Admin} />
           <Route exact path="/user" component={User} />
           <Route component={NoMatch} />
