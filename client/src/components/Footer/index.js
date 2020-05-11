59e8194a1a77035391a6dc6a5621ebc2c511b1ef
Robert Finkley <rfinkley82@gmail.com>
2020-05-11 17:46:47 -0400

    Added text-center class to Footer component

--- client/src/components/Footer/index.js
+++ client/src/components/Footer/index.js
@@ -2,7 +2,7 @@ import React from "react";
 
 export function Footer() {
   return (
-    <div className="container-fluid fixed-bottom">
+    <div className="container-fluid fixed-bottom text-center">
         <div className="row">
             <div className="col-sm-12">
                 <a className="navbar-brand" href="/">Pager&copy; </a>
