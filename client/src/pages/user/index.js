d54939724097e3afd77fc44daa1b58c1161cc175
Robert Finkley <rfinkley82@gmail.com>
2020-05-11 17:48:14 -0400

    Created user page with dynamic content

--- client/src/pages/user/index.js
+++ client/src/pages/user/index.js
@@ -1,15 +1,61 @@
 import React, { Component } from "react";
+import { Col, Container, Row } from "../../components/Grid";
+import Nav from "../../components/Nav";
+
 
 class user extends Component {
+  state={
+    isAdmin: false,
+  }
   render() {
-    return (
-      <div className="user">
-        <div className="jumbotron">
-            <h1 className="text-dark">
-                User page.
-            </h1>
+
+  const renderAdminView = () => {
+      if (this.state.isAdmin) {
+        return (
+          <div className="wrapper">
+            <Nav />
+            <Container fluid>
+              <Row>
+                <Col size="sm-12 md-3">
+                  <h3>This is the admin view</h3>
+                  <ul>
+                    <li>Create Schedule</li>
+                    <li>Edit Schedule</li>
+                    <li>Page On-Call Staff</li>
+                  </ul>
+                </Col>
+                <Col size="sm-12 md-9">
+                  <h1>This is where the schedule will go</h1>
+                </Col>
+              </Row>
+            </Container>
+          </div>
+      )} else {
+        return (
+          <div className="wrapper">
+          <Nav />
+          <Container fluid>
+            <Row>
+              <Col size="sm-12 md-3">
+                <h3>This is the general user view</h3>
+                <ul>
+                  <li>Page On-Call Staff</li>
+                </ul>
+              </Col>
+              <Col size="sm-12 md-9">
+                <h1>This is where the schedule will go</h1>
+              </Col>
+            </Row>
+          </Container>
         </div>
-      </div>
+        );
+      } 
+    }
+
+  return (
+    <div>
+      {renderAdminView()}
+    </div>
     );
   }
 }
