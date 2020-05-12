d86d33b86d0fdde162bde9e6aa3aa48f7d44036b
BRANDEN PATTEN <patten.branden@gmail.com>
2020-05-12 05:30:00 -0400

    added the route to post a schedule in it's current form. Schedule should be passed to user page as a state with no issues.

--- controllers/scheduleController.js
+++ controllers/scheduleController.js
@@ -14,6 +14,47 @@ module.exports = {
       .catch((err) => res.status(422).json(err));
   },
   create: function (req, res) {
+    //when the shift starts, ends, and who it's ment for.
+    let startDate = req.body.startDate
+    let endDate = req.body.endDate
+    let schedulee = req.body.schedulee
+
+    scheduleObj = {
+      startDate: startDate,
+      endDate: endDate
+    }
+
+    let newSchedule = new Schedule(scheduleObj)
+    newSchedule.save(function(err, sched){
+      if (err) {
+        console.log(err)
+      } else {
+        console.log(sched._id)
+        console.log(schedulee)
+
+        db.Schedule.findOneAndUpdate(
+          //Local the user the schedule is for
+          {email: schedulee},
+          //push the refference id to this user's schedule array in the DB
+          { $push: {schedules: sched._id } },
+          //and that this is a new id we are adding
+          {new: true}
+          //then error log if something went wrong, or tell the browser everything is gucci
+        ).exec(function(err){ 
+          if (err) {
+            console.log(err);
+          } else {
+            console.log(success)
+            res.end(303)
+          }
+        })
+      }
+    })
+
+
+    
+
+
     db.Schedule.create(req.body)
       .then((dbModel) => res.json(dbModel))
       .catch((err) => res.status(422).json(err));
