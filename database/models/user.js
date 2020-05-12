cf3ece40fae16ce917184eec8479a5cd4ce13b03
BRANDEN PATTEN <patten.branden@gmail.com>
2020-05-12 06:33:10 -0400

    modified user db model, isAdmin is true by default when user is created

--- database/models/user.js
+++ database/models/user.js
@@ -7,7 +7,7 @@ const userSchema = new Schema({
   email: { type: String, required: true },
   firstName: { type: String, required: true },
   lastName: { type: String, required: true },
-  isAdmin: { type: Boolean },
+  isAdmin: { type: Boolean, default: true },
   schedules: [
     {
       type: Schema.Types.ObjectId,
