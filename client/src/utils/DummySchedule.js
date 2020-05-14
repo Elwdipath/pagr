7e39acb1f2872df2c5e204f57b7a1a90aa42ac43
Robert Finkley <rfinkley82@gmail.com>
2020-05-14 11:26:40 -0400

    Updated dummy data

--- client/src/utils/DummySchedule.js
+++ client/src/utils/DummySchedule.js
@@ -1,8 +1,8 @@
 let dummy = [
     {
         title: 'Dr. Smith On-Call',
-        start: '2020-05-11T20:30:00',
-        end: '2020-05-11T23:30:00',
+        start: '2020-05-13T20:30:00',
+        end: '2020-05-13T23:30:00',
         extendedProps: {
             slackUserID: 'dsmithMD123',
             email: 'dsmith@med.med'
@@ -10,8 +10,8 @@ let dummy = [
     },
     {
         title: 'Dr. Smith On-Call',
-        start: '2020-05-11T10:30:00',
-        end: '2020-05-11T11:50:00',
+        start: '2020-05-13T10:30:00',
+        end: '2020-05-13T11:50:00',
         extendedProps: {
             slackUserID: 'dsmithMD123',
             email: 'dsmith@med.med'
