4fcafe33756f7c1e7d270cafec6e91438184c33a
BRANDEN PATTEN <patten.branden@gmail.com>
2020-05-17 06:03:34 -0400

    added email field and timestamps.

--- database/models/schedule.js
+++ database/models/schedule.js
@@ -2,9 +2,13 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const scheduleSchema = new Schema({
+  email: { type: String, required: true},
   startDate: { type: Date, required: true },
   endDate: { type: Date, required: true },
   
+},
+{
+  timestamps: true
 });
 
 const Schedule = mongoose.model("Schedule", scheduleSchema);
