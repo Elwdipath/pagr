c08ec14706583319b65e3ae693fd5f19829fabc8
BRANDEN PATTEN <patten.branden@gmail.com>
2020-05-17 06:43:12 -0400

    updated routes, model, and API,  to handle incoming schedule post requests

--- database/models/schedule.js
+++ database/models/schedule.js
@@ -2,9 +2,14 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const scheduleSchema = new Schema({
-  startDate: { type: Date, required: true },
-  endDate: { type: Date, required: true },
+  email: { type: String, required: true },
+  date: { type: Date, required: true },
+  startTime: { type: Date, required: true },
+  endTime: { type: Date, required: true },
   
+},
+{
+  timestamps: true
 });
 
 const Schedule = mongoose.model("Schedule", scheduleSchema);
