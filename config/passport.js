87cbf41f332eaaf88dbea92ba24838053d1d5985
Jordan Kaiser <jordan.kaiser@gmail.com>
2020-05-21 22:27:46 -0400

    started work on slack ids

--- config/passport.js
+++ config/passport.js
@@ -1,22 +1,26 @@
 const passport = require("passport");
 const LocalStrategy = require("passport-local").Strategy;
 const db = require("../database/models");
+const messageController = require("../controllers/messageController");
 
 // registration handler
 passport.use(
   "local-signup",
-  new LocalStrategy(
-    {
+  new LocalStrategy({
       emailField: "email",
       passReqToCallback: true,
     },
     function (req, email, password, done) {
-      db.User.findOne({ email: email }).then(function (err, user) {
+      db.User.findOne({
+        email: email
+      }).then(function (err, user) {
         if (err) {
           return done(err);
         }
         if (user) {
-          return done(null, false, { message: "email is already in use." });
+          return done(null, false, {
+            message: "email is already in use."
+          });
         }
         db.User.create({
           email: email,
@@ -32,16 +36,33 @@ passport.use(
 // login handler
 passport.use(
   "local-login",
-  new LocalStrategy(
-    { usernameField: "email" },
+  new LocalStrategy({
+      usernameField: "email"
+    },
     function (email, password, done) {
-      db.User.findOne({ email: email }, (err, user) => {
+      db.User.findOne({
+        email: email
+      }, (err, user) => {
+        // console.log(user)
         if (!user) {
-          return done(null, false, { message: "email incorrect" });
+          return done(null, false, {
+            message: "email incorrect"
+          });
         }
         if (!user.checkPassword(password)) {
-          return done(null, false, { message: "Incorrect password" });
+          return done(null, false, {
+            message: "Incorrect password"
+          });
         }
+        if (!user.slackUserID) {
+          let userEmail = user.email
+          messageController.getUsers(userEmail)
+        }
+        // else {
+        //   let email = user
+        //   messageController.getUsers(user.email)
+        // }
+
         return done(null, user);
       });
     }
