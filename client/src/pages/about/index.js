dc624ad0bba02364ec283f2d9488ae668e433340
Robert Finkley <rfinkley@gmail.com>
2020-05-26 18:25:02 -0400

    Updated Contact Us page

--- client/src/pages/about/index.js
+++ client/src/pages/about/index.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import Nav from "../../components/Nav";
 import { Footer } from "../../components/Footer";
-import {Form, Container, Row, Col } from "react-bootstrap";
+import {Card, Container, Row, Col } from "react-bootstrap";
 
 class About extends Component {
   constructor(props) {
@@ -40,26 +40,55 @@ class About extends Component {
             <a className="nav-link" href="/contact">Contact Us</a>
           </li>
         </Nav>
-        <Container className="mt-3">
+        <Container className="mt-3 text-center">
           <h1>Meet the Team!</h1>
-          <Row className="mt-3">
-            <Col sm={12} md={3} className='text-center mt-4'>
-              <img src="assets/img/jordan.png" alt="" height="150px" width="150px"/>
-              <h1>Jordan</h1>
-              <p>Jordan was a great project lead. He made sure we stayed on course. If there was a obstacle he helped us work through it. He has a strong head for front-end or back-end problem solving.</p>
+          <Row className="mt-3 justify-center">
+          <Col sm={12} md={3} className='text-center m-4'>
+              <Card style={{ width: '18rem', height: '32rem' }}>
+                <Card.Img variant="top" src="assets/img/jordan.png" style={{height: '200px'}} />
+                <Card.Body>
+                  <Card.Title>Jordan Kaiser</Card.Title>
+                  <Card.Text>
+                    Jordan was a great project lead. He made sure we stayed on course. If there was a obstacle he helped us work through it. He has a strong head for front-end or back-end problem solving.
+                  </Card.Text>
+                </Card.Body>
+                <Card.Body>
+                  <Card.Link href="https://github.com/Elwdipath">GitHub</Card.Link>
+                  <Card.Link href="https://www.linkedin.com/in/jordan-kaiser-6332666/">LinkedIn</Card.Link>
+                </Card.Body>
+              </Card>
             </Col>
-            <Col sm={12} md={3} className='text-center mt-4'> 
-              <img src="assets/img/branden.jpg" alt="Branden Pic" height="150px" width="150px"/>
-              <h1>Branden</h1>
-              <p>Branden is a back-end wizard. He was repsonsible for giving us a solid backend foundation. He worked two jobs and still managed to get us setup ahead of schedule. We could not have done this project without him.</p>
+            <Col sm={12} md={3} className='text-center m-4'>
+              <Card style={{ width: '18rem', height: '32rem' }}>
+                <Card.Img variant="top" src="assets/img/branden.jpg" style={{height: '200px'}} />
+                <Card.Body>
+                  <Card.Title>Branden Patten</Card.Title>
+                  <Card.Text>
+                    Branden is a back-end wizard. He was repsonsible for giving us a solid backend foundation. He worked two jobs and still managed to get us setup ahead of schedule. We could not have done this project without him.
+                  </Card.Text>
+                </Card.Body>
+                <Card.Body>
+                  <Card.Link href="https://github.com/pattenbranden">GitHub</Card.Link>
+                  <Card.Link href="https://www.linkedin.com/in/branden-patten/">LinkedIn</Card.Link>
+                </Card.Body>
+              </Card>
             </Col>
-            <Col sm={12} md={3} className='text-center mt-4'>
-            <img src="assets/img/robert.JPG" alt="Robert Pic" height="150px" width="150px"/>
-              <h1>Robert</h1>
-              <p>Robert handled the front-end build. He learned more about React modals than he ever wanted to learn. </p>
+            <Col sm={12} md={3} className='text-center m-4'>
+              <Card style={{ width: '18rem', height: '32rem' }}>
+                <Card.Img variant="top" src="assets/img/robert.jpg" style={{height: '200px'}} />
+                <Card.Body>
+                  <Card.Title>Robert Finkley</Card.Title>
+                  <Card.Text>
+                    Robert handled the front-end build. He learned more about React modals than he ever wanted to learn. 
+                  </Card.Text>
+                </Card.Body>
+                <Card.Body>
+                  <Card.Link href="https://github.com/rfinkley">GitHub</Card.Link>
+                  <Card.Link href="https://www.linkedin.com/in/robert-finkley-a0252517/">LinkedIn</Card.Link>
+                </Card.Body>
+              </Card>
             </Col>
           </Row>
-
         </Container>
         <Footer />
       </div>
