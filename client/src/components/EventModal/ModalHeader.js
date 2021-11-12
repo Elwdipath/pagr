58c3c434e03f5603a585ab9a1383c1c50a88ac85
Jordan Kaiser <Jordan.kaiser@gmail.com>
2021-11-12 16:49:36 +0000

    test

--- client/src/components/EventModal/ModalHeader.js
+++ client/src/components/EventModal/ModalHeader.js
@@ -5,7 +5,7 @@ import ModalHeader from "react-bootstrap/ModalHeader";
 export function EventModalHeader(props, {children}) {
   
     return (
-      // <!-- Modal -->    
+       
           <ModalHeader {...props}>
             {children}
           </ModalHeader>
